c3134dbe615365befb76b08ae087b95fbbfaacd5
MasahiroTomita1002 <masahiro.tomita@ntt.com>
2014-10-08 09:53:25 +0900

    Update SpeaktoitRecognitionServiceImpl.java
    Audio data added

--- ailib/src/main/java/ai/api/SpeaktoitRecognitionServiceImpl.java
+++ ailib/src/main/java/ai/api/SpeaktoitRecognitionServiceImpl.java
@@ -25,6 +25,12 @@ import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.util.Log;
+
+//ADD20141001
+import android.media.AudioTrack;
+import android.media.AudioManager;
+//ADD20141001
 
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
@@ -38,7 +44,6 @@ import java.util.Calendar;
 public class SpeaktoitRecognitionServiceImpl extends AIService {
 
     public static final String TAG = SpeaktoitRecognitionServiceImpl.class.getName();
-
     private static final int SAMPLE_RATE_IN_HZ = 16000;
     private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
     private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
@@ -49,25 +54,68 @@ public class SpeaktoitRecognitionServiceImpl extends AIService {
 
     private ByteArrayOutputStream outputStream;
 
+    //ADD20141001
+    private final AudioTrack audioTrack;
     protected SpeaktoitRecognitionServiceImpl(final Context context, final AIConfiguration config) {
         super(config);
 
-        mediaRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                SAMPLE_RATE_IN_HZ,
-                CHANNEL_CONFIG,
-                AUDIO_FORMAT,
-                AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ, CHANNEL_CONFIG, AUDIO_FORMAT));
+                mediaRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                                                   SAMPLE_RATE_IN_HZ,
+                                                   CHANNEL_CONFIG,
+                                                   AUDIO_FORMAT,
+                                                   AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ, CHANNEL_CONFIG, AUDIO_FORMAT));
+
+        //ADD20141001
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,// 音楽ストリームを設定
+                                      SAMPLE_RATE_IN_HZ,// サンプルレート
+                                      AudioFormat.CHANNEL_CONFIGURATION_MONO,// モノラル
+                                      AUDIO_FORMAT,//16BIT
+                                      AudioTrack.getMinBufferSize(SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_CONFIGURATION_MONO, AUDIO_FORMAT),// バッファ・サイズ                                      AudioTrack.MODE_STREAM);// Streamモード。データを書きながら再生する
     }
 
     @Override
     public void startListening() {
-        outputStream = new ByteArrayOutputStream();
+        outputStream      = new ByteArrayOutputStream();
 
         mediaRecorder.startRecording();
         isRecording = true;
 
-        onListeningStarted();
+        // 録音Thread
+        new Thread(new Runnable() {
+            //@Override
+            int bufSize = mediaRecorder.getMinBufferSize(SAMPLE_RATE_IN_HZ,
+                    CHANNEL_CONFIG,
+                    AUDIO_FORMAT);
 
+            public void run() {
+                int readsize;
+                byte buf[] = new byte[bufSize];
+
+                /*
+                String filePath = Environment.getExternalStorageDirectory() + "/soundData.wav";
+                File file = new File(filePath);
+                file.getParentFile().mkdir();
+                */
+                try{
+                    //fileoutputStream = new  FileOutputStream(file);
+
+                    while (isRecording == true) {
+                        // 録音データ読み込み
+                        readsize = mediaRecorder.read(buf, 0, buf.length);
+
+                        if (readsize > 0) {
+                            outputStream.write(buf, 0, readsize);
+                            //fileoutputStream.write(buf, 0, readsize);
+                        }
+                    }
+                    //再生
+                    //audioTrack.play();
+                    //audioTrack.write(outputStream.toByteArray(), 0, outputStream.toByteArray().length);
+                }catch (Exception e){
+                }
+            }
+        }).start();
+        onListeningStarted();
     }
 
     @Override
@@ -77,6 +125,13 @@ public class SpeaktoitRecognitionServiceImpl extends AIService {
             isRecording = false;
 
             onListeningFinished();
+            /*
+            try {
+                fileoutputStream.close();
+            }catch (Exception e){
+
+            }
+            */
 
             sendRequest(outputStream.toByteArray());
         }
@@ -96,7 +151,6 @@ public class SpeaktoitRecognitionServiceImpl extends AIService {
     private void sendRequest(final byte[] soundData) {
         try {
             final URL url = new URL(config.getQuestionUrl());
-
             final QuestionMetadata questionMetadata = new QuestionMetadata();
             questionMetadata.setLanguage(config.getLanguage());
             questionMetadata.setAgentId(config.getAgentId());
@@ -111,6 +165,7 @@ public class SpeaktoitRecognitionServiceImpl extends AIService {
                 protected void onPostExecute(final String stringResult) {
                     try {
                         final AIResponse aiResponse = GsonFactory.getGson().fromJson(stringResult, AIResponse.class);
+                        //int ka = aiResponse.setTimestamp();
                         onResult(aiResponse);
 
                     } catch (final Exception e) {
